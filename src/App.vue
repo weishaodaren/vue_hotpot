973880960117f8f471a3f19c1313f87575c7b111
weishaodaren <45391716+weishaodaren@users.noreply.github.com>
2019-03-10 21:50:29 +0800

    更新组件

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <template>
   <div id="app">
-  <h1>App.vue</h1>
   <router-view/>
   </div>
 </template>
+
+<script>
+  export default{
+    mounted(){
+      console.log('来来甜甜圈');
+      //当根组件挂载完成后异步请求整个网站全局配置数据
+      var url=this.$store.state.globalSettings.apiUrl
+      +'/admin/settings';
+      this.$axios.get(url).then((res)=>{
+        this.$store.commit('setGlobalSettings',res.data);
+      }).catch((err)=>{
+        console.log(err);
+      })
+    },
+  }
+</script> 
 
 <style lang="scss">
   #app{
