460e26a3a5c4f5b70dd237fdab1d57bd27b08729
weishaodaren <45391716+weishaodaren@users.noreply.github.com>
2019-03-10 21:54:11 +0800

    添加面包屑 样式 点击跳转

--- src/views/GlobalSettings.vue
+++ src/views/GlobalSettings.vue
@@ -1,5 +1,33 @@
 <template>
     <div>
         <h1>GlobalSetitngs</h1>
+        <el-breadcrumb>
+            <el-breadcrumb-item to="/main">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>全局设置</el-breadcrumb-item>
+        </el-breadcrumb>
+<br>
+        <el-card shadow="never">
+            <el-form label-width="100px">
+              <el-form-item label="应用名称:">
+                <el-input v-model="formData.appName"></el-input>    
+              </el-form-item>  
+            </el-form>
+        </el-card>
     </div>
 </template>
+
+<script>
+export default {
+    data(){
+        return{
+            formData:{
+
+            }
+        }
+    }
+}
+</script>
+
+
+
+
